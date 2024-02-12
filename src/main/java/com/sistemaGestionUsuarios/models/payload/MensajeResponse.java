b72c79629ab75370f61ced85a58beb11fcfc279a
Rhabad <nicolaspizarro.450@gmail.com>
2024-02-12 16:39:56 -0300

    payload: mensaje, objeto

---
+++ src/main/java/com/sistemaGestionUsuarios/models/payload/MensajeResponse.java
@@ -0,0 +1,10 @@
+package com.sistemaGestionUsuarios.models.payload;
+
+import lombok.Builder;
+import lombok.Data;
+import lombok.ToString;
+
+@Data @ToString
+@Builder
+public class MensajeResponse {
+}
